723ff8889edbcc141b03040a84ebab83a7390625
ctfang <chenyz@wecut.com>
2019-09-30 18:16:23 +0800

    first commit

---
+++ app/bean.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://swoft.org/docs
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+use Swoft\Db\Pool;
+use Swoft\Http\Server\HttpServer;
+use Swoft\Task\Swoole\TaskListener;
+use Swoft\Task\Swoole\FinishListener;
+use Swoft\Rpc\Server\ServiceServer;
+use Swoft\Server\SwooleEvent;
+use Swoft\Db\Database;
+
+return [
+    'noticeHandler'      => [
+        'logFile' => '@runtime/logs/notice-%d{Y-m-d-H}.log',
+    ],
+    'applicationHandler' => [
+        'logFile' => '@runtime/logs/error-%d{Y-m-d}.log',
+    ],
+    'logger'            => [
+        'flushRequest' => false,
+        'enable'       => false,
+        'json'         => false,
+    ],
+    'httpServer'        => [
+        'class'    => HttpServer::class,
+        'port'     => 8888,
+        'listener' => [
+            'rpc' => bean('rpcServer')
+        ],
+        'process'  => [
+//            'monitor' => bean(MonitorProcess::class)
+//            'crontab' => bean(CrontabProcess::class)
+        ],
+        'on'       => [
+//            SwooleEvent::TASK   => bean(SyncTaskListener::class),  // Enable sync task
+            SwooleEvent::TASK   => bean(TaskListener::class),  // Enable task must task and finish event
+            SwooleEvent::FINISH => bean(FinishListener::class)
+        ],
+        /* @see HttpServer::$setting */
+        'setting' => [
+            'task_worker_num'       => 12,
+            'task_enable_coroutine' => true,
+            'worker_num'            => 6
+        ]
+    ],
+    'httpDispatcher'    => [
+        // Add global http middleware
+        'middlewares'      => [
+            \App\Http\Middleware\FavIconMiddleware::class,
+            // \Swoft\Whoops\WhoopsMiddleware::class,
+            // Allow use @View tag
+            \Swoft\View\Middleware\ViewMiddleware::class,
+        ],
+        'afterMiddlewares' => [
+            \Swoft\Http\Server\Middleware\ValidatorMiddleware::class
+        ]
+    ],
+    'db'                => [
+        'class'    => Database::class,
+        'dsn'      => 'mysql:dbname=test;host=127.0.0.1',
+        'username' => 'root',
+        'password' => '123456',
+    ],
+    'db.pool' => [
+        'class'    => Pool::class,
+        'database' => bean('db'),
+    ],
+    'migrationManager'  => [
+        'migrationPath' => '@database/Migration',
+    ],
+    'rpcServer'         => [
+        'class' => ServiceServer::class,
+    ],
+];
